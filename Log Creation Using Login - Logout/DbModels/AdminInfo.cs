f3672d76e062c25fdbc090449fd842b17b496c0c
mdrakibhasan126568 <mdrakibhasan126568@gmail.com>
2026-01-21 17:37:09 +0600

    Add project files.

---
+++ Log Creation Using Login - Logout/DbModels/AdminInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Log_Creation_Using_Login___Logout.DbModels;
+
+public partial class AdminInfo
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
---
+++ Log Creation Using Login - Logout/DbModels/RegesterServiceContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Log_Creation_Using_Login___Logout.DbModels;
+
+public partial class RegesterServiceContext : DbContext
+{
+    public RegesterServiceContext()
+    {
+    }
+
+    public RegesterServiceContext(DbContextOptions<RegesterServiceContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AdminInfo> AdminInfos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=192.168.10.202;Database=Regester_Service;User Id=sa;Password=123456;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AdminInfo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__AdminInf__3214EC0780892F8E");
+
+            entity.ToTable("AdminInfo");
+
+            entity.Property(e => e.Email).HasMaxLength(30);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(6);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
